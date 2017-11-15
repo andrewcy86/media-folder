2f95188dc7f454d82e352c1fc849c750dec7dffe
Andrew Yuen <andrewcy86@users.noreply.github.com>
2017-11-15 13:31:02 -0500

    Update class-media-folder-type.php

--- includes/class-media-folder-type.php
+++ includes/class-media-folder-type.php
@@ -55,7 +55,7 @@ class Media_Folder_Type {
         $args = array(
             'labels'              => $labels,
           //'capabilities'        => $capabilities,
-            'capability_type'     => 'read',
+            'capability_type'     => 'post',
             'menu_icon'           => 'dashicons-category',
             'hierarchical'        => true,
             'supports'            => array('title','thumbnail'),
